269f1c98af833d66a564a685a21f2829af9c3aa7
sourourmarz <sourourah@gmail.com>
2022-03-15 10:58:54 +0100

    first commit

---
+++ state-management-with-ngxs-app/src/app/app.module.ts
@@ -0,0 +1,32 @@
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+import {NgxsModule} from '@ngxs/store';
+import { ReactiveFormsModule } from '@angular/forms';
+import { UserFormComponent } from './components/user-form/user-form.component';
+import { UserListComponent } from './components/user-list/user-list.component';
+import { UserState } from './states/user.state';
+import { environment } from 'src/environments/environment';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    UserFormComponent,
+    UserListComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgxsModule.forRoot([UserState], {
+      developmentMode: !environment.production
+    }),
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
